fd324a45800e894d206e63a184b627b36cbeb845
Delilah <ila0406@gmail.com>
2022-05-11 18:32:41 -0600

    added css loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,47 +5,42 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-module: {
-    // CSS loaders
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        // We use babel-loader in order to use ES6.
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-          },
-        },
-      },
-    ],
-  },
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+        main: './src/js/index.js',
+        install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
       
     ],
 
     module: {
-      rules: [
-        
-      ],
+        // CSS loaders
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                // We use babel-loader in order to use ES6.
+                use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+                },
+                },
+            },
+        ],
     },
   };
 };
